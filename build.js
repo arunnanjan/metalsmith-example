1b3e1b7afba8b91a0f5c2c1786c351402beab702
Josh Williams <vmizzle@gmail.com>
2016-11-07 12:18:32 -0500

    Initial commit.

---
+++ build.js
@@ -0,0 +1,46 @@
+/*
+ * Pull in Metalsmith and necessary plugins.
+ */
+var metalsmith = require('metalsmith'),
+    layouts = require('metalsmith-layouts'),
+    markdown = require('metalsmith-markdownit');
+
+/*
+ * Start the metalsmith build pipeline.  Give it the current directory to work with.
+ */
+metalsmith(__dirname)
+  /*
+   * Setup the site metadata.  More on this later.
+   */
+  .metadata({
+    site: {
+      title: 'Metalsmith Awesomeness'
+    }
+  })
+  /*
+   * Tell Metalsmith where our site's code lives
+   */
+  .source('src')
+  /*
+   * And where to place the build artifacts
+   */
+  .destination('build')
+  /*
+   * Process our markdown.
+   */
+  .use(markdown('commonmark', { html: true }))
+  /*
+   * Add some structure to the generated pages.
+   */
+  .use(layouts({
+    engine: 'pug',
+    directory: 'layouts',
+    pretty: true
+  }))
+  /*
+   * Finally, magic time.  Build us some internets.
+   */
+  .build(function(err, files) {
+    if (err) { throw err; }
+    console.log('Build complete.');
+  });
---
+++ src/about.md
@@ -0,0 +1,9 @@
+---
+title: About Me
+date: 2016-10-24
+author: Average Joe
+layout: main.pug
+---
+## About Me
+
+I build sites with Metalsmith.
